74ac798b8ee0ea8e8de7d8e28021d69e8ce509d9
nirvale <nirvale@gmail.com>
2024-02-15 19:47:26 -0600

    Revert "Merge branch 'main' of https://github.com/nirvale/sare"
    This reverts commit a1fdd82d1521b82fc118810bee3ee79689227a81, reversing
    changes made to 4ff2310369bee3c86d9ecca7e9e4a933e518ab40.

--- database/seeders/NicSeeder.php
+++ database/seeders/NicSeeder.php
@@ -4,16 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Tnic;
 
-class TnicSeeder extends Seeder
+class NicSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $tnic = Tnic::create(['tnic' => 'FÍSICA']);
-        $tnic = Tnic::create(['tnic' => 'VIRTUAL']);
+        //
     }
 }
