e6a97c2636e95cfd69877222f421d149219e2bd2
nirvale <nirvale@gmail.com>
2024-02-15 20:38:43 -0600

    actualizacion modelos dbam

--- app/Models/Dbam/Bdiaria.php
+++ app/Models/Dbam/Bdiaria.php
@@ -4,6 +4,9 @@ namespace App\Models\Dbam;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\Belongs;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Bdiaria extends Model
 {
